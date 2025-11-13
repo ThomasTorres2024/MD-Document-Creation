d052039d89c5bc920f049516ec5100052df551fc
Thomas <thomastorres2024@gmail.com>
2025-11-13 08:46:56 -0500

    initializing everything will add this as a cloud function later

---
+++ create_templates.py
@@ -0,0 +1,33 @@
+
+
+#Incoming JSON String Format 
+#Document Type - invoice, 
+# Invoice specific fields - dict of str 
+#   date - Date of Issue 
+#   descriptions - list[dict[str]] work description - 
+#       desc - desc 
+#       quantity - quantity
+#       unit price - unity price
+#       total - total
+# subtotal - subtotal  
+# tax - tax 
+# total 
+#
+#optional fields 
+#expiration/validation date 
+#expected start date
+#expected completion date
+#terms conditionsa and exclusions 
+#notes/additional details 
+#work auth signature 
+
+#for the first part im just going to focus ong etting the eky stuff in 
+
+
+def get_translated_document(fields : dict):
+    
+    #determine document type 
+    
+    
+    
+    pass
